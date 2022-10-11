4fc8d6f8430036b63ff3d37bfbcecbdced622eb7
Wesley Charles Batschauer <108537192+WesleyBatschauer@users.noreply.github.com>
2022-10-11 11:28:17 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,44 @@ using namespace std;
 #include <locale.h>
 #include "A.h"
 
-int main() {
-  agenda<comercial,100> ag1;
+struct pessoal{
+  int cel, ID;
+  long int CPF;
+  string nome;
+};
+
+struct comercial{
+  int ID, tel;
+  long int CNPJ;
+  string nome_emp;
+};
+
+int main(){
+  agenda<pessoal,100> ag1;
+  agenda<comercial,100> ag2;
+  int opcao;
+
+  cout<<"Vc quer qual agenda ?";
+  cout<<"";
+  while(opcao != 6){
+    switch(opcao) {
+      case 1:
+        
+    }
+  }
+  /*agenda<pessoal,100> ag1;
   int dado;
   
-  inicializa_agenda(ag1);
+  //inicializa_agenda(ag1);
 
   cout<<"Digite uma função:\n";
   cin>>dado;
 
-    insere(ag1);
+  cout<<"Digite uma CPF:\n";
+  cin>>ag1.itens[0].CPF;
+
+  cout<<endl<<ag1.itens[0].CPF;
+
+    //insere(ag1);*/
   
 }
