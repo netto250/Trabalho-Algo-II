fcca7508ed517cd42014bbf169363ad5b884c522
Wesley Charles Batschauer <108537192+WesleyBatschauer@users.noreply.github.com>
2022-10-13 11:25:56 -0300

    Update A.h

--- A.h
+++ A.h
@@ -1,6 +1,3 @@
-#ifndef _A_H_
-#define _A_H_
-
 template <typename TIPO, int MAX>
   struct agenda {
     TIPO itens[MAX];
@@ -9,49 +6,37 @@ template <typename TIPO, int MAX>
 
 // Inicializa a agenda. Inicializações: Define que agenda está sem nenhum item
 template <typename TIPO, int MAX>
-bool inicializa_agenda(agenda <TIPO,MAX> &ag) {
+void inicializa_agenda(agenda <TIPO,MAX> &ag) {
   ag.quantidade = 0;
-  return true;
-};
+}
 
 // insere o item na agenda (protótipo)
 // Retorna true caso consiga inserir o dado na agenda, false caso contrário
 template <typename TIPO, int MAX>
-void insere(agenda <TIPO,MAX> &ag1){
-  
-  if(ag1.itens[0].verif==true){
-    insere_COM(ag1);
-  }
-  
-  if(ag1.itens[0].verif==false){
-    insere_COM(ag1);
-  }
+bool insere(agenda <TIPO,MAX> &ag, TIPO dado) {
+    ag.itens[ag.quantidade]=dado;
+    ag.quantidade++;
+    return true;
+}
+
+void remover(agenda <TIPO,MAX> &ag,int ID_pesquisa){
+    for(int i=0;i<ag.quantidade;i++){
+        cout<<"\n\n"<<ag.itens[i].CPF;
+        cout<<"\n"<<ag.itens[i].nome;
+        cout<<"\n"<<ag.itens[i].ID;
+        cout<<"\n"<<ag.itens[i].cel
+    }
 }
 
 template <typename TIPO, int MAX>
-void insere_COM(agenda <TIPO,MAX> &ag){
-  {
-    cout<<"digite o CNPJ";
-    cin>>ag.itens[0].CNPJ;
-    cout<<"digite o NOME";
-    cin>>ag.itens[0].nome;
-    cout<<"digite o TEL";
-    cin>>ag.itens[0].tel;
-    cout<<"digite o ID";
-    cin>>ag.itens[0].ID;
+void imprime(agenda <TIPO,MAX> &ag) {
+  int i=0;
+  for(i=0;i<ag.quantidade;i++){
+    cout<<"\n\n"<<ag.itens[i].CPF;
+    cout<<"\n"<<ag.itens[i].nome;
+    cout<<"\n"<<ag.itens[i].ID;
+    cout<<"\n"<<ag.itens[i].cel<<"\n\n";
   }
-}
-
-template <typename TIPO, int MAX>
-void insere_PES(agenda <TIPO,MAX> &ag){
-    cout<<"digite o CPF";
-    cin>>ag.itens[0].CPF;
-    cout<<"digite o NOME";
-    cin>>ag.itens[0].nome;
-    cout<<"digite o CEL";
-    cin>>ag.itens[0].cel;
-    cout<<"digite o ID";
-    cin>>ag.itens[0].ID;
 }
 
 #endif
